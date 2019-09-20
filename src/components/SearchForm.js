43345ad8783bec6f36a5b220f815bf55c41f2d06
Adam Payne <ajpayne238@gmail.com>
2019-09-20 14:04:13 -0400

    not mvp..

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import React, {useState, useEffect} from "react";
 
+import CharacterList from "./CharacterList";
+
 // function SearchForm() {
  
 //   return (
